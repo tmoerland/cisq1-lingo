6b700969d320bd4be6044c7c5ea008417b85d4fa
thomoerland <thom.moerland@student.hu.nl>
2021-02-19 15:51:29 +0100

    +Added "attempt" attribute +Added Lombok EqualsAndHasCode and ToString annotation

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -1,17 +1,23 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
+@EqualsAndHashCode
+@ToString
 public class Feedback {
     private final List<Mark> marks;
+    private String attempt; //TODO: update domainmodel
 
-    public Feedback(List<Mark> marks) {
+    public Feedback(List<Mark> marks, String attempt) {
         this.marks = marks;
+        this.attempt = attempt;
     }
 
     public boolean isWordGuessed(){
