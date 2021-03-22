2c3e917dcfec33071f53d6b70ef7416ea12f6e9c
thomoerland <thom.moerland@student.hu.nl>
2021-03-22 13:40:55 +0100

    Added AttemptToMarks helper

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/helper/AttemptToMarks.java
@@ -0,0 +1,29 @@
+package nl.hu.cisq1.lingo.trainer.helper;
+
+import nl.hu.cisq1.lingo.trainer.domain.Mark;
+import nl.hu.cisq1.lingo.trainer.domain.exception.InvalidAttemptException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AttemptToMarks {
+    public static List<Mark> giveMarks(String wordToGuess, String attempt){
+        List<Mark> marks = new ArrayList<>();
+
+        if (wordToGuess.length() != attempt.length()){
+            throw new InvalidAttemptException();
+        }
+
+        for(int i = 0; i < wordToGuess.length(); i++){
+            if(wordToGuess.charAt(i) == attempt.charAt(i)) {
+                marks.add(Mark.CORRECT);
+            }
+            else if (wordToGuess.indexOf(attempt.charAt(i)) != -1) {  //if the attempt char does not occur, -1 is returned
+                marks.add(Mark.PRESENT);                         //so everything but -1 means the char is present
+            } else {
+                marks.add(Mark.ABSENT);
+            }
+        }
+        return marks;
+    }
+}
