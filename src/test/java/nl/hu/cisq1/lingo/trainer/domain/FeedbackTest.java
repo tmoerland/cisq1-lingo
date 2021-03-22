9743af6f5d934a1894ecec2172a3d8a3b8ff4b02
thomoerland <thom.moerland@student.hu.nl>
2021-03-22 13:41:53 +0100

    Replaced "woord" with "apple" to keep the code English

--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -3,8 +3,12 @@ package nl.hu.cisq1.lingo.trainer.domain;
 import nl.hu.cisq1.lingo.trainer.domain.exception.InvalidFeedbackException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,14 +16,14 @@ public class FeedbackTest {
     @Test
     @DisplayName("Word is guessed if all letters are correct")
     void wordIsGuessed(){
-        Feedback feedback = new Feedback(List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT), "woord");
+        Feedback feedback = new Feedback(List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT), "apple");
         assertTrue(feedback.isWordGuessed());
     }
 
     @Test
     @DisplayName("Word is not guessed if one or more letters are incorrect")
     void wordIsNotGuessed() {
-        Feedback feedback = new Feedback(List.of(Mark.CORRECT, Mark.CORRECT, Mark.ABSENT, Mark.CORRECT, Mark.CORRECT), "woord");
+        Feedback feedback = new Feedback(List.of(Mark.CORRECT, Mark.CORRECT, Mark.ABSENT, Mark.CORRECT, Mark.CORRECT), "apple");
         assertFalse(feedback.isWordGuessed());
     }
 
