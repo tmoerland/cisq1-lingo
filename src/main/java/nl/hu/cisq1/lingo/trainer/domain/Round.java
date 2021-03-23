d09fa2e698f5a0e9507f12c106209485248974a4
thomoerland <thom.moerland@student.hu.nl>
2021-03-23 16:38:42 +0100

    Added Round logic

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Round.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Round.java
@@ -1,19 +1,56 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
 import lombok.Getter;
+import nl.hu.cisq1.lingo.trainer.exception.InvalidAttemptException;
 import nl.hu.cisq1.lingo.trainer.exception.RoundException;
-import nl.hu.cisq1.lingo.trainer.helper.AttemptToMarks;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-public class Round {
-    private final Word wordToGuess;
-    private int attempts;
+public class Round extends LingoRules{
 
-    public Round(Word wordToGuess) {
+    private final String wordToGuess;
+    private String previousHint;
+    private LingoStatus status = LingoStatus.PLAYING;
+    List<Feedback> feedbackList = new ArrayList<>();
+
+    public Round(String wordToGuess) {
         this.wordToGuess = wordToGuess;
-        this.attempts = 0;
+        this.previousHint = this.giveFirstHint(wordToGuess);
+    }
+
+    public Feedback attempt(String attempt) {
+        this.checkRoundValidity(attempt);
+
+        Feedback feedback = new Feedback(Feedback.giveMarks(wordToGuess, attempt), attempt);
+        feedbackList.add(feedback);
+
+        if (attempt.equals(wordToGuess)) {
+            status = LingoStatus.ROUND_OVER;
+        }
+        if (feedbackList.size() == 5 && status != LingoStatus.ROUND_OVER){
+            status = LingoStatus.PLAYER_LOST;
+        }
+
+        this.previousHint = feedback.giveHint(this.previousHint);
+        return feedback;
+    }
+
+    public int checkNumberOfAttempts(){
+        return feedbackList.size();
+    }
+
+    private String giveFirstHint(String wordToGuess){
+        return wordToGuess.charAt(0) + ".".repeat(wordToGuess.length());
+    }
+
+    private void checkRoundValidity(String attempt){    //checks if the round isn't already over
+        if(!this.getStatus().equals(LingoStatus.PLAYING)){
+            throw new RoundException(); //players shouldn't get here in the first place!
+        }
+        if (attempt.length() != wordToGuess.length()){  //checks if the attempt has the same nr. of chars as the word to guess
+            throw new InvalidAttemptException();
+        }
     }
 }
