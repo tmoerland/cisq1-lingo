918923e4e6788952180812358b1b261d97d1fc30
thomoerland <thom.moerland@student.hu.nl>
2021-03-23 14:33:44 +0100

    Added LingoRules for keeping global settings

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/LingoRules.java
@@ -0,0 +1,8 @@
+package nl.hu.cisq1.lingo.trainer.domain;
+
+public class LingoRules {
+    /*
+     * How often the player is allowed to make a guess each round
+     */
+    public static final int MAX_ATTEMPTS = 5;
+}
